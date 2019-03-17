25423ef5eac586b7cab6045c9c01290077eae5b3
Jose Trinidad <internick10@yahoo.com.mx>
2019-03-17 16:59:19 -0600

    second commit crud mongo

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,14 +1,16 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://internick14:Mibruno10@cluster0-2wmrb.mongodb.net/test?retryWrites=true');
+mongoose.connect(
+  "mongodb+srv://internick14:Mibruno10@cluster0-2wmrb.mongodb.net/test?retryWrites=true"
+);
 
 let CustomerSchema = new mongoose.Schema({
-    name: String,
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  name: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
 });
 
-module.exports = mongoose.model('Customer', CustomerSchema);
+module.exports = mongoose.model("Customer", CustomerSchema);
--- routes/customer.js
+++ routes/customer.js
@@ -1,37 +1,79 @@
-let express = require('express');
-let CustomerModel = require('../models/customer.model');
-let router = express.Router();  
+let express = require("express");
+let CustomerModel = require("../models/customer.model");
+let router = express.Router();
 
-router.post('/customer', (req, res) => {
-    if(!req.body){
-        return res.status(400).send('Request body is missing');
-    }
-    let model = new CustomerModel(req.body)
-    model.save()
+//Post method
+router.post("/customer", (req, res) => {
+  if (!req.body) {
+    return res.status(400).send("Request body is missing");
+  }
+  let model = new CustomerModel(req.body);
+  model
+    .save()
     .then(doc => {
-        if(!doc || doc.length === 0) {
-            return res.status(500).send(doc)
-        }
-        res.status(201).send(doc)
+      if (!doc || doc.length === 0) {
+        return res.status(500).send(doc);
+      }
+      res.status(201).send(doc);
     })
     .catch(err => {
-        res.status(500).json(err)
+      res.status(500).json(err);
+    });
+});
+
+//Get method
+router.get("/customer", (req, res) => {
+  if (!req.query.email) {
+    return res.status(400).send("Missing url parameter: email");
+  }
+  CustomerModel.findOne({
+    email: req.query.email
+  })
+    .then(doc => {
+      res.json(doc);
     })
-})
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
-module.exports = router; 
+//Put method
+router.put("/customer", (req, res) => {
+  if (!req.query.email) {
+    return res.status(400).send("Missing url parameter: email");
+  }
+  CustomerModel.findOneAndUpdate(
+    {
+      email: req.query.email
+    },
+    req.body,
+    {
+      new: true
+    }
+  )
+    .then(doc => {
+      res.json(doc);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+//delete method
+router.delete("/customer", (req, res) => {
+  if (!req.query.email) {
+    return res.status(400).send("Missing url parameter: email");
+  }
+  CustomerModel.findOneAndDelete({
+    email: req.query.email
+  })
+    .then(doc => {
+      res.json(doc);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = router;
